2aabfe2aada9447ee68c4a6efd860a174f889d1a
Starinin Andrey <starininandrey@gmail.com>
2021-01-13 18:45:43 +0300

    Написание программы

--- FileAndDirectory/Program.cs
+++ FileAndDirectory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileAndDirectory
 {
@@ -6,7 +7,25 @@ namespace FileAndDirectory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DriveInfo[] discs = DriveInfo.GetDrives();
+            foreach (DriveInfo disc in discs)
+            {
+                Console.Write($"{disc.Name} [{disc.VolumeLabel}] - ");
+                Console.Write($"{disc.DriveFormat}, {disc.DriveType} ");
+                Console.Write($"{disc.TotalSize}, {disc.TotalFreeSpace}");
+
+                var files = Directory.GetFiles(disc.Name);
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                var directories = Directory.GetDirectories(disc.Name);
+                foreach (var directoriy in directories)
+                {
+                    Console.WriteLine(directoriy);
+                }
+            }
         }
     }
 }
